eb953108bba617df06614e8e7af0352f244911ac
ashish-gadiya-bs <ashish.gadiya@bigsteptech.com>
2014-10-01 10:09:47 +0530

    Add ClassNamespaceSniff class for Class Namespace rule

--- src/Socialengine/SnifferRules/Standard/SocialEngine/Sniffs/Classes/ClassDeclarationSniff.php
+++ src/Socialengine/SnifferRules/Standard/SocialEngine/Sniffs/Classes/ClassDeclarationSniff.php
@@ -2,8 +2,6 @@
 
 class SocialEngine_Sniffs_Classes_ClassDeclarationSniff implements PHP_CodeSniffer_Sniff
 {
-    protected $ignoreNamespace = [];
-
     /**
      * Returns an array of tokens this test wants to listen for.
      *
@@ -11,7 +9,6 @@ class SocialEngine_Sniffs_Classes_ClassDeclarationSniff implements PHP_CodeSniff
      */
     public function register()
     {
-        $this->ignoreNamespace = \Config::get('sniffer-rules::ignoreNamespace', []);
         return [
             T_CLASS,
             T_INTERFACE,
@@ -30,12 +27,6 @@ class SocialEngine_Sniffs_Classes_ClassDeclarationSniff implements PHP_CodeSniff
      */
     public function process(PHP_CodeSniffer_File $phpcsFile, $stackPtr)
     {
-        $this->processSingleClass($phpcsFile, $stackPtr);
-        $this->processHavingNamespace($phpcsFile, $stackPtr);
-    }
-
-    public function processSingleClass(PHP_CodeSniffer_File $phpcsFile, $stackPtr)
-    {
         $tokens = $phpcsFile->getTokens();
         $errorData = array(strtolower($tokens[$stackPtr]['content']));
 
@@ -44,34 +35,5 @@ class SocialEngine_Sniffs_Classes_ClassDeclarationSniff implements PHP_CodeSniff
             $error = 'Each %s must be in a file by itself';
             $phpcsFile->addError($error, $nextClass, 'MultipleClasses', $errorData);
         }
-    }
-
-    public function processHavingNamespace(PHP_CodeSniffer_File $phpcsFile, $stackPtr)
-    {
-        $fileName = $phpcsFile->getFilename();
-        if ($this->shouldIgnoreMissingNamespace($fileName)) {
-            return;
-        }
-
-        $tokens = $phpcsFile->getTokens();
-        $errorData = array(strtolower($tokens[$stackPtr]['content']));
-        $namespace = $phpcsFile->findPrevious(T_NAMESPACE, ($stackPtr - 1));
-        if ($namespace === false) {
-            $error = 'Each %s must be in a namespace of at least one level (a top-level vendor name)';
-            $phpcsFile->addError($error, $stackPtr, 'MissingNamespace', $errorData);
-        }
-    }
-
-    protected function shouldIgnoreMissingNamespace($filename)
-    {
-        $isIgnore = false;
-        foreach ($this->ignoreNamespace as $ignored) {
-            if (strpos($filename, $ignored) === 0) {
-                $isIgnore = true;
-                break;
-            }
-        }
-
-        return $isIgnore;
     }
 }
